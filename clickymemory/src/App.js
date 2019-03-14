665e111dd503e58e335945059ba6d58c4883b39e
oliver.renwick@gmail.com <oliver.renwick@gmail.com>
2019-03-13 22:12:42 -0400

    stripped out lots of the unneeded functionality. Tried to build a randomizing function for the friends array. Needs debugging.

--- clickymemory/src/App.js
+++ clickymemory/src/App.js
@@ -4,33 +4,39 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 
+function randomize(friends) {
+  var n = friends.length;
+  var tempArr = [];
+  for ( var i = 0; i < n-1; i++ ) {
+    // The following line removes one random element from arr
+    // and pushes it onto tempArr
+    tempArr.push(friends.splice(Math.floor(Math.random()*friends.length),1)[0]);
+  }
+  // Push the remaining item onto tempArr
+  tempArr.push(friends[0]);
+  friends=tempArr;
+  console.log(friends)
+  // Return it back to the app
+  return friends;
+}
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>Clicky-Clicky Memory Game</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            onClick={randomize()}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
